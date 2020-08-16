6c31e959284c03c48af8b4f951c33757cf5c8938
samesense <samesense@gmail.com>
2020-08-16 13:33:25 -0400

    Resource

--- tagit.sh
+++ tagit.sh
@@ -1,2 +1,9 @@
 git tag --delete 1.0.0
 git push --delete origin 1.0.0
+
+git add -A
+git commit -m 'Resource'
+git tag -a 1.0.0 `git log -1 | head -1 | cut -f 2 -d ' '` -m "Resource"
+
+git push origin master
+git push origin 1.0.0
