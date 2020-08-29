399ca6daf5656ddecb40ccc156216ff2d7c5a6e4
samesense <samesense@gmail.com>
2020-08-29 08:51:05 -0400

    Resource

--- tagit.sh
+++ tagit.sh
@@ -5,5 +5,5 @@ git add -A
 git commit -m 'Resource'
 git tag -a 1.0.0 `git log -1 | head -1 | cut -f 2 -d ' '` -m "Resource"
 
-git push origin chop
+git push origin wxs-revert
 git push origin 1.0.0
